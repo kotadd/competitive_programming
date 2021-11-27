12299aee8f4c9a23e36744d645d9b4ad235b5e75
Kota Nishinaka <nishinaka4business@gmail.com>
2021-11-28 00:15:46 +0900

    add: template

---
+++ template.py
@@ -0,0 +1,26 @@
+import sys
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+sys.setrecursionlimit(10 ** 7)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+
+def main():
+    input = sys.stdin.readline
+
+
+if __name__ == '__main__':
+    main()
